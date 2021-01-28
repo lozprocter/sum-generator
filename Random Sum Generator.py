8a8395d92a180c75052be9597f69203a81a48eae
lozprocter <72624353+lozprocter@users.noreply.github.com>
2021-01-28 19:48:27 +0000

    Add files via upload

--- Random Sum Generator.py
+++ Random Sum Generator.py
@@ -4,7 +4,7 @@ while True:
     try:
         first = randint(0,100)
         second = randint(0,100)
-
+        #enerates question and takes user input (answer)
         print(first, "+", second)
         answer = input("Answer:", )
 
@@ -13,5 +13,6 @@ while True:
             print("Good Job!")
         else:
             print("Bad Luck")
+        #protects the program from crashing from a non-numerical input
     except ValueError:
         print("Not a valid input")
